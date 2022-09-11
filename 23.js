a9220418de27de0db674b23fee0e14d46b756f42
smselimmuna102706 <111196494+smselimmuna102706@users.noreply.github.com>
2022-09-11 20:00:52 +0600

    Tarnary-Oparator

---
+++ 23.js
@@ -0,0 +1,16 @@
+
+
+// var number = Number(prompt("Enter the number :"));
+
+// var result = number>0 ? "positive" :"negative";
+
+
+// document.write(result);
+
+
+var number = Number(prompt("Enter the number :"));
+
+var result = number>0 ? "positive" :number<0 ?"negative":"zero";
+
+
+document.write(result);
